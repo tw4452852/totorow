2ad75407f2c7215849546622a5ef65812feefa14
Tw <tw19881113@gmail.com>
2012-12-02 23:05:10 +0800

    add log

--- app/controllers/data.go
+++ app/controllers/data.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"github.com/robfig/revel"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"time"
 )
@@ -24,7 +23,7 @@ func (d DataPlugin) OnAppStart() {
 	//assue data dir is in ../
 	const topDir = "./data/"
 	if err := filepath.Walk(topDir, func(path string, info os.FileInfo, err error) error {
-		rev.INFO.Println(path)
+		rev.ERROR.Println(path)
 		if path == topDir {
 			return nil
 		}
@@ -36,8 +35,6 @@ func (d DataPlugin) OnAppStart() {
 	}); err != nil {
 		rev.ERROR.Printf("walk %q failed: %s\n", topDir, err)
 	}
-	o, _ := exec.Command("pwd").CombinedOutput()
-	records = append(records, &Record{time.Now().String(), string(o)})
 }
 
 func init() {
